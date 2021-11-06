fffb01cc14dd3804ea6641c18120a6f208dc0428
radusuciu <radusuciu@gmail.com>
2021-11-06 14:50:42 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-mfzdqn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-mfzdqn)
---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div>hi {{ name }}</div>
+    <input type="text" :value="name" />
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    name: 'tester',
+    wat: 'lol',
+  }),
+};
+</script>
